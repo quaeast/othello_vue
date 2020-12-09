fac789f9dbde6db5512356bd68b952712bdede12
fangzidong <424044876@qq.com>
2020-12-09 13:52:46 +0800

    居中

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,10 +1,15 @@
 <template>
     <span>选择模式 1 代表 AI，0 代表人。</span>
     <div>
+        <span>黑 </span>
         <input v-model="playerStatus[0]">
+        <br>
+        <span>白 </span>
         <input v-model="playerStatus[1]">
     </div>
+    <div class="boardWrapper">
     <Board :playerStatus="playerStatus"></Board>
+    </div>
 </template>
 
 <script>
@@ -21,5 +26,8 @@
 </script>
 
 <style scoped>
-
+    .boardWrapper{
+        display: flex;
+        justify-content: center;
+    }
 </style>
