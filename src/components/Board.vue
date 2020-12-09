0f1dff79b319ed2d024c108af377b8140aa1707f
fangzidong <424044876@qq.com>
2020-12-09 14:56:15 +0800

    增加步骤提示和落子限制

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -14,7 +14,7 @@
     import Token from "./Token";
     import axios from 'axios';
 
-    function mapToColor(colorMatrix) {
+    function mapToColor(colorMatrix, enableMatrix) {
         let result = new Array(8);
         for (let i = 0; i < 8; i++) {
             result[i] = new Array(8);
@@ -26,6 +26,9 @@
                 } else {
                     result[i][j] = "rgba(255,255,255,0)";
                 }
+                if (enableMatrix[i][j]===1){
+                    result[i][j] = "rgba(255,0,0,1)";
+                }
             }
         }
         return result;
@@ -36,7 +39,8 @@
     }
 
 
-    let statusMatrix = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, -1, 1, 0, 0, 0], [0, 0, 0, 1, -1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
+    const statusMatrix = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, -1, 1, 0, 0, 0], [0, 0, 0, 1, -1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
+    // const zeroMatrix = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
 
     export default {
         name: "Board",
@@ -49,7 +53,9 @@
         },
         data: function () {
             return {
+
                 statusMatrix: statusMatrix,
+                enableMatrix: [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]],
                 position: null,
                 // 当前执棋者身份，0黑棋（-1），1白棋（1）
                 currentPlayer: 0,
@@ -57,7 +63,7 @@
         },
         computed: {
             colorMatrix: function () {
-                return mapToColor(this.statusMatrix);
+                return mapToColor(this.statusMatrix, this.enableMatrix);
             },
         },
         methods: {
@@ -89,6 +95,7 @@
                                 "action": response.data["action"]
                             }
                         ).then(function (response) {
+                            currentThis.enableMatrix = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
                             currentThis.statusMatrix = response.data["board"];
                             currentThis.currentPlayer = (1 + currentThis.currentPlayer) % 2;
                         })
@@ -96,6 +103,9 @@
             },
             humanRun: function () {
                 if (this.playerStatus[this.currentPlayer] === 1) {
+                    return;
+                }
+                if (this.enableMatrix[this.position[0]][this.position[1]]===0){
                     return;
                 }
                 console.log("human run");
@@ -109,13 +119,32 @@
                         "action": action
                     }
                 ).then(function (response) {
+                    currentThis.enableMatrix = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
                     currentThis.statusMatrix = response.data["board"];
                     currentThis.currentPlayer = (1 + currentThis.currentPlayer) % 2;
+                })
+            },
+            fetchValidPosition: function () {
+                const currentThis = this;
+                axios.post(
+                    'http://47.240.25.164:5000/valid',
+                    {
+                        "board": currentThis.statusMatrix,
+                        "cur_player": mapToColorNum(currentThis.currentPlayer),
+                    }
+                ).then(function (response) {
+                    const validArray = response.data["valid_actions"];
+                    console.log(validArray);
+                    for (const position of validArray) {
+                        console.log(position);
+                        currentThis.enableMatrix[position[0]][position[1]]  = 1;
+                    }
                 })
             }
         },
         watch: {
             currentPlayer: function () {
+                this.fetchValidPosition();
                 this.AIRun();
             },
             position: function () {
@@ -126,6 +155,7 @@
         },
         created() {
             this.AIRun();
+            this.fetchValidPosition();
         }
     }
 </script>
